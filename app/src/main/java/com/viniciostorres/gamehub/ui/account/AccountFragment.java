7390cc8187c5e9e8ecf3ab118a582196aa364b98
ViniciosTorres-Dev <vinicios.santos.torres@gmail.com>
2026-02-05 21:43:08 -0300

    Chore: Activities, Fragments and XML archives created.

---
+++ app/src/main/java/com/viniciostorres/gamehub/ui/account/AccountFragment.java
@@ -0,0 +1,6 @@
+package com.viniciostorres.gamehub.ui.account;
+
+import androidx.fragment.app.Fragment;
+
+public class AccountFragment extends Fragment {
+}
---
+++ app/src/main/java/com/viniciostorres/gamehub/ui/login/LoginFragment.java
@@ -0,0 +1,6 @@
+package com.viniciostorres.gamehub.ui.login;
+
+import androidx.fragment.app.Fragment;
+
+public class LoginFragment extends Fragment {
+}
